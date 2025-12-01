1e871957516a6e163b8065829d47202fd072e1d3
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-12-01 13:37:18 +0600

    developed error_handler_middleware+utilty_fuction

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { type Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalError.middleware';
+import notFound from './app/middlewares/notFound.middleware';
 
 dotenv.config();
 
 export const app: Application = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//  not found route handler
+app.use(notFound)
 
+// global error handler
+app.use(globalErrorHandler);
