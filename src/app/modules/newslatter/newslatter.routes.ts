ad3e896fb11cd34feda2e9cebc6e7dc897872c2e
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-12-22 19:32:31 +0600

    developed newslatter service action

--- src/app/modules/newslatter/newslatter.routes.ts
+++ src/app/modules/newslatter/newslatter.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import { UserRole } from "../../../generated/prisma/enums";
+import { auth } from "../../middlewares/auth";
 import { NewsLatterController } from "./newslatter.controller";
 
 const router = Router()
 
 router.get("/", NewsLatterController.getAllFromDB)
 
-router.post("/", NewsLatterController.insertIntoDB)
+router.post("/", auth(UserRole.ADMIN), NewsLatterController.insertIntoDB)
 
 
 export const NewsLatterRoutes = router
